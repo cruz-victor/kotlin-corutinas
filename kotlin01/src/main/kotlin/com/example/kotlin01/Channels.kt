5912353af31468759102e64a7284d38ba8fddd13
Vic <cruz.gomez.victor@gmail.com>
2024-04-09 10:38:34 -0400

    send and receive from channel example

--- kotlin01/src/main/kotlin/com/example/kotlin01/Channels.kt
+++ kotlin01/src/main/kotlin/com/example/kotlin01/Channels.kt
@@ -3,6 +3,16 @@ package com.example.kotlin01
 fun main(){
     print("Hellow vic")
     channelsBasic()
+    formasEnviarElementosChannel()
+    formasRecivirElementosChannel()
+}
+
+fun formasRecivirElementosChannel() {
+    TODO("Not yet implemented")
+}
+
+fun formasEnviarElementosChannel() {
+    TODO("Not yet implemented")
 }
 
 fun channelsBasic() {
