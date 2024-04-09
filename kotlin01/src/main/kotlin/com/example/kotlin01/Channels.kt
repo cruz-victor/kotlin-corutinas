2ebe35b235e3115887e5ea98857db1e80b50a4ac
Vic <cruz.gomez.victor@gmail.com>
2024-04-09 11:18:52 -0400

    new lines added

--- kotlin01/src/main/kotlin/com/example/kotlin01/Channels.kt
+++ kotlin01/src/main/kotlin/com/example/kotlin01/Channels.kt
@@ -1,41 +1,47 @@
 package com.example.kotlin01
 
 fun main(){
-    print("Hellow vic")
-    channelsBasic()
-    formasEnviarElementosChannel()
-    formasRecivirElementosChannel()
-    formasCerrarChannel()
-    pipelines()
-    bufferChannel()
+//    channelsBasic()
+//    formasEnviarElementosChannel()
+//    formasRecivirElementosChannel()
+//    formasCerrarChannel()
+//    pipelines()
+//    bufferChannel()
     manejoExcepcionesChannel()
 
 }
 
 fun manejoExcepcionesChannel() {
-    newTopic("manejoExcepcionesChannel")
+//    newTopic("manejoExcepcionesChannel")
+    println("manejoExcepcionesChannel")
 }
 
 fun bufferChannel() {
-    newTopic("bufferChannel")
+//    newTopic("bufferChannel")
+    println("bufferChannel")
 }
 
 fun pipelines() {
-    newTopic("pipelines")
+//    newTopic("pipelines")
+    println("pipelines")
 }
 
 fun formasCerrarChannel() {
-    newTopic("formasCerrarChannel")
+//    newTopic("formasCerrarChannel")
+    println("formasCerrarChannel")
 }
 
 fun formasRecivirElementosChannel() {
-    newTopic("formasRecivirElementosChannel")
+//    newTopic("formasRecivirElementosChannel")
+    println("formasRecivirElementosChannel")
 }
 
 fun formasEnviarElementosChannel() {
-    newTopic("formasEnviarElementosChannel")
+//    newTopic("formasEnviarElementosChannel")
+    println("formasEnviarElementosChannel")
 }
 
 fun channelsBasic() {
-    newTopic("channelsBasic")
+//    newTopic("channelsBasic")
+    println("channelsBasic")
 }
