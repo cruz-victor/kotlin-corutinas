cdd8322796b5ad6d5550006b1d019b3d6c7d6717
Vic <cruz.gomez.victor@gmail.com>
2024-04-10 17:29:09 -0400

    basic channel example

--- kotlin01/src/main/kotlin/com/example/kotlin01/CoroutinesAdvance.kt
+++ kotlin01/src/main/kotlin/com/example/kotlin01/CoroutinesAdvance.kt
@@ -1,13 +1,17 @@
 package com.example.kotlin01
 
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 fun main(){
-//    channelsBasic()
+    basicChannel()
 //    formasEnviarElementosChannel()
 //    formasRecivirElementosChannel()
 //    formasCerrarChannel()
 //    pipelines()
 //    bufferChannel()
-    manejoExcepcionesChannel()
+//    manejoExcepcionesChannel()
 
 }
 
@@ -41,7 +45,26 @@ fun formasEnviarElementosChannel() {
     println("formasEnviarElementosChannel")
 }
 
-fun channelsBasic() {
-//    newTopic("channelsBasic")
-    println("channelsBasic")
+val countries = listOf("La Paz", "Cochabamba","Santa Cruz")
+
+fun basicChannel() {
+    runBlocking {
+        newTopic("channelsBasic")
+        val channel= Channel<String>()
+
+        launch {
+            countries.forEach {
+                channel.send(it)
+            }
+        }
+
+//        repeat(3){
+//            println(channel.receive())
+//        }
+
+        for(value in channel){
+            println(channel.receive()) //No funciona porque los channel estan disenados para solucionar concurrencia entre coroutines
+            //println(value)
+        }
+    }
 }
